1730da636ed4d5890e49f0a84cf5a57de6180924
aidynoJ <azhakizhanov@ebsco.com>
2024-01-05 19:15:04 +0600

    reduce test fixtures

--- test/fixtures/holdingsRecords.js
+++
@@ -1,5 +0,0 @@
-export const holdingsRecords = [
-  {
-    id: '44a21d17-a666-4f34-b24d-644f8ed1537b',
-  }
-];
--- test/fixtures/relationshipTypes.js
+++
@@ -1,6 +0,0 @@
-import { instanceRelationshipTypes } from './instanceRelationshipTypes';
-
-export const relationshipTypes = instanceRelationshipTypes.map(it => ({
-  label: it.name,
-  value: it.id,
-}));
