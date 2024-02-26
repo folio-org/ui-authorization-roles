d3a1155aa85c4f9cd8b92244caac91fbede9d07c
aidynoJ <azhakizhanov@ebsco.com>
2024-02-26 17:36:31 +0600

    test: use capability sets test coverage

--- src/hooks/useCapabilitySets.js
+++ src/hooks/useCapabilitySets.js
@@ -1,24 +1,20 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useQuery } from 'react-query';
 
-import { useNamespace, useOkapiKy, useStripes } from '@folio/stripes/core';
-import { getCapabilitiesGroupedByTypeAndResource } from '../settings/utils';
+import { useNamespace, useOkapiKy } from '@folio/stripes/core';
+
+const LIMIT = 5000;
 
 const useCapabilitySets = () => {
   const ky = useOkapiKy();
-  const stripes = useStripes();
   const [namespace] = useNamespace({ key: 'capability-sets' });
 
   const { data, isSuccess } = useQuery(
     namespace,
-    () => ky.get(`capability-sets?limit=${stripes.config.maxUnpagedResourceCount}&query=cql.allRecords=1 sortby resource`).json(),
+    () => ky.get(`capability-sets?limit=${LIMIT}&query=cql.allRecords=1 sortby resource`).json(),
   );
 
-  const groupedCapabilitiesByType = useMemo(() => {
-    return getCapabilitiesGroupedByTypeAndResource(data?.capabilitySets || []);
-  }, [data]);
-
-  return { groupedCapabilitiesByType, data, isSuccess };
+  return { data, isSuccess };
 };
 
 export default useCapabilitySets;
