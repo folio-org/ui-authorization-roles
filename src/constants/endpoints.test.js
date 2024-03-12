c64290f42aec6ccf7a63696314fc60e2f41d4627
Zak Burke <zburke@ebsco.com>
2024-03-12 15:29:41 -0400

    fix tests
    I'm not a fan of tests that, essentially, evaluate a constant :|

--- src/constants/endpoints.test.js
+++ src/constants/endpoints.test.js
@@ -2,6 +2,6 @@ import { ROLES_ENDPOINT } from './endpoints';
 
 describe('Endpoints', () => {
   it('test roles endpoint without search term', () => {
-    expect(ROLES_ENDPOINT(1000)).toBe('roles?limit=1000&query=cql.allRecords=1 sortby id');
+    expect(ROLES_ENDPOINT(1000)).toMatch(/roles\?limit=1000&query=cql.allRecords=1/);
   });
 });
